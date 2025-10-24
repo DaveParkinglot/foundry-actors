295160fec1445be1dd648ff3c273a684f6d44a18
Jordan Nolte <jk.nolte@gmail.com>
2025-10-24 09:56:30 -0400

    Set up Foundry Actor viewer

--- script.js
+++ script.js
@@ -1,21 +1,33 @@
-const actors = ["ExampleActor"]; // List of your actor JSON filenames without extension
-
+const actorFolder = "foundry-actors/";
 const select = document.getElementById("actorSelect");
-actors.forEach(actor => {
-  const option = document.createElement("option");
-  option.value = actor;
-  option.textContent = actor;
-  select.appendChild(option);
-});
+const sheetDiv = document.getElementById("sheet");
 
-select.addEventListener("change", () => loadActor(select.value));
+// Fetch list of JSON files from GitHub Pages
+// Since we are static, we can pre-generate a JSON index file
+fetch(`${actorFolder}index.json`)
+  .then(res => res.json())
+  .then(actors => {
+    // Populate dropdown
+    actors.forEach(name => {
+      const option = document.createElement("option");
+      option.value = name;
+      option.textContent = name;
+      select.appendChild(option);
+    });
 
+    // Load first actor by default
+    loadActor(actors[0]);
+  })
+  .catch(err => {
+    sheetDiv.innerHTML = `<p style="color:red;">Could not load actor list: ${err}</p>`;
+  });
+
+// Load actor JSON and render
 async function loadActor(name) {
-  const sheetDiv = document.getElementById("sheet");
   sheetDiv.innerHTML = "Loading...";
 
   try {
-    const res = await fetch(`actors/${name}.json`);
+    const res = await fetch(`${actorFolder}${name}.json`);
     if (!res.ok) throw new Error("Actor not found");
     const actor = await res.json();
 
@@ -29,9 +41,4 @@ async function loadActor(name) {
     }
     sheetDiv.innerHTML = html;
   } catch(err) {
-    sheetDiv.innerHTML = `<p style="color:red;">${err.message}</p>`;
-  }
-}
-
-// Load first actor by default
-loadActor(actors[0]);
+    sheetDiv.innerHTML = `<p style="color:
